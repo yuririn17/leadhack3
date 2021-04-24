d665c35ca7074618254a2023644f5b5085f49ed0
yuririn17 <snowman14abe@gmail.com>
2021-04-24 12:43:06 +0900

    Init:project

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
 console.log("Helloworld");
 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+ res.send('Hello World!');
+});
+
+//app.listen(3000, function () {
+//});
+
+(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
